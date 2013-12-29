f715eafd040e4dd0704ec3d86bf2a3a225f3f59d
xingrui <ryan@xingruideMBP.local>
2013-12-30 06:59:20 +0800

    edit setup file

---
+++ .vim/setup.bash
@@ -0,0 +1,7 @@
+git clone https://github.com/tpope/vim-pathogen.git autoload/
+git clone https://github.com/davidhalter/jedi-vim.git bundle/jedi-vim
+git clone https://github.com/LaTeX-Box-Team/LaTeX-Box.git bundle/latex-box
+git clone https://github.com/scrooloose/nerdtree.git bundle/nerdtree
+git clone https://github.com/vim-scripts/YankRing.vim.git bundle/yankring
+git clone https://github.com/vim-scripts/taglist.vim.git bundle/taglist
+git clone https://github.com/ervandew/supertab.git bundle/supertab
